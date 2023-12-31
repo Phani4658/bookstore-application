207b2c6a83bdf0acee0ee755ca8345c6f7fb341e
Phani <officialphani.cse@gmail.com>
2023-12-31 09:21:58 +0530

    Modify Routes to v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { Component } from 'react'
-import './App.css'
-import { Switch,Route,Redirect } from 'react-router-dom'
-import Home from './components/Home'
-import BookList from './components/BookList'
-import BookDetails from './components/BookDetails'
-import Cart from './components/Cart'
-import NotFound from './components/NotFound'
-import Checkout from './components/Checkout'
-class App extends Component{
-  render(){
-    return(
-      <Switch>
-        <Route exact path='/' component = {Home} />
-        <Route exact path='/books' component = {BookList} />
-        <Route exact path='/books/:id' component = {BookDetails} />
-        <Route exact path="/cart" component = {Cart} />
-        <Route exact path="/checkout" component = {Checkout} />
-        <Route exact path="/not-found" component = {NotFound} />
-        <Redirect to='/not-found' />
-      </Switch>
-    )
+import { Component } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom'; // Changed 'redirect' to 'Navigate'
+import './App.css';
+import Home from './components/Home';
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+import Cart from './components/Cart';
+import NotFound from './components/NotFound';
+import Checkout from './components/Checkout';
+
+class App extends Component {
+  render() {
+    return (
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/books" element={<BookList />} />
+        <Route exact path="/books/:id" element={<BookDetails />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/not-found" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/not-found" />} /> {/* Use Navigate to redirect */}
+      </Routes>
+    );
   }
 }
 
-export default App
+export default App;
